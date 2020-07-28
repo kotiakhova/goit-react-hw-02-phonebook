6afe7031ab32686df64f9be34d4cf7e28b876ac8
kotiakhova <kotiakhovaa@gmail.com>
2020-07-28 18:43:34 +0300

    Updates

---
+++ precache-manifest.315408c2e95de8e704915041c94a832d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905e94827ea2af5952661ec36c19d7b5",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "e101554b95606371cf76",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.ad6d0beb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.ad6d0beb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe407d08ecd9d375506",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.0e01c989.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
